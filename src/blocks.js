036a70e41565e831ee6ff5262b2237bb8e576a99
Honza Bambas :mayhemer <honzab@firemni.cz>
2019-03-03 21:52:55 +0100

    correct the default link destination to media

--- src/blocks.js
+++ src/blocks.js
@@ -9,16 +9,36 @@
  * Webpack is compiling as the input file.
  */
 
+import './common/block.js';
 import './art/block.js';
 import './stepbystep/block.js';
 
 wp.hooks.addFilter(
   'blocks.registerBlockType',
-  'my-plugin/class-names/list-block',
+  'fdd-blocks/common/hook',
   (properties, name) => {
-    //console.log(name);
-    //console.log(properties);
     if (name === 'core/image') {
+      console.log(`registration`);
+      console.log(properties);
+
+      let edit = properties.edit;
+      properties.edit = function () {
+        console.log(`hook to core/image.edit() ENTER`);
+        console.log(arguments);
+
+        let block = arguments[0];
+        let { attributes } = block;
+        block.setAttributes({
+          linkDestination: 'media',
+          href: attributes.url,
+          caption: '',
+        });
+
+        let result = edit.apply(this, arguments);
+
+        console.log(`hook to core/image.edit() LEAVE`);
+        return result;
+      }
     }
     return properties;
   }
